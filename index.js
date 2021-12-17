f73b5e74221e4f16734304e49481d49234294a1e
MikolaiSib <75933871+MikolaiSib@users.noreply.github.com>
2021-12-17 19:39:57 +0700

    Three

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function shortcut(string){
-    let word = ''
-    for (let i = 0; i < string.length; i++) {     
-      if (string[i] == 'q' | string[i] == 'e' | string[i] == 'y' | string[i] == 'u' | string[i] == 'i' | string[i] == 'o' | string[i] == 'a') 
-      continue
-      else {word += string[i]}      
-      
-  }
-    return word
-  } 
+function getAge(inputString){
+  return parseInt(inputString[0])
+}

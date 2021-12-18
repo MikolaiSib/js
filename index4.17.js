974c86a1d662e3368c371229d940133c968c1b0a
MikolaiSib <75933871+MikolaiSib@users.noreply.github.com>
2021-12-18 15:12:08 +0700

    4.17

--- index4.17.js
+++ index4.17.js
@@ -1,4 +1,10 @@
-function move (position, roll) {
-  return (position+roll+roll)
-  // return the new position
+function expressionMatter(a, b, c) {
+  let s1 = a*b*c
+  let s2 = a+b+c
+  let s3 = a*(b+c)
+  let s4 = (a+b)*c
+  let s5 = a*b+c
+  let s6 = a+b*c
+  let myMax =Math.max(s1,s2,s3,s4,s5,s6);
+  return(myMax)
 }

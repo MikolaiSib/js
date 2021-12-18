a82f6e3b8709be115ec34656056f73e438c20d36
MikolaiSib <75933871+MikolaiSib@users.noreply.github.com>
2021-12-18 13:11:46 +0700

    4.4

---
+++ index4.4.js
@@ -0,0 +1,11 @@
+function howMuchILoveYou(nbPetals) {
+  nbPetals %=6
+  if (nbPetals==1) {return 'I love you'}
+  if (nbPetals==2) {return 'a little'}
+  if (nbPetals==3) {return 'a lot'}
+  if (nbPetals==4) {return 'passionately'}
+  if (nbPetals==5) {return 'madly'}
+  if (nbPetals==0) {return 'not at all'}
+  
+    // your code
+}
